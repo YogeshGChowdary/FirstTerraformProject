6cdb33d3fc52ed960b0f948543f08bb994adcf88
YogeshGChowdary <yogesh.g.chowdary@gmail.com>
2024-07-14 19:18:47 +0530

    folder management

--- AbhishekCourse/4/backend.tf
+++ AbhishekCourse/4/backend.tf
@@ -1,6 +1,6 @@
 # you cannot directly use variables inside the terraform block when defining the Terraform backend configuration. Instead, you need to hard-code the values for the backend configuration.
 
-terraform {
+terraform { 
   backend "s3" {
     bucket = "backendbucketcheck"     
     key = "yogesh/terraform.tfstate"
